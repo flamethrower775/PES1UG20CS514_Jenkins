c7a35636e06a9dbdf69c3486342aaada33b6e42a
Vrinda Girimaji <girimaji.vrinda@gmail.com>
2023-02-15 13:16:49 +0530

    Commit4_task5

---
+++ new_cpp_file.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "this works PES1UG20CS514" << std::endl;
+    std::cout << "lala i like lala" << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << (i+5) << std::endl;
+    }
+    std::cout << "done" << std::endl;
+    return 0;
+}
