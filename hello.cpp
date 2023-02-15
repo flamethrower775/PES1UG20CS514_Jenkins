b14b84cf45465582d07b3f85970b48503243f9a8
Vrinda Girimaji <girimaji.vrinda@gmail.com>
2023-02-15 12:31:49 +0530

    Commit2_task4

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,9 @@
 
 int main(void) {
     std::cout << "This is Vrinda: PES1UG20CS514" << std::endl;
+    std::cout << "Lala" << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
